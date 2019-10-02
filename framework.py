fae974cba2a430010ea2785f6a44e96053689f6d
Jacob <jacobmen416@yahoo.com>
2019-10-01 23:17:25 -0500

    initial setup

---
+++ framework.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+# only temp. replace with real templates once ready
+#@app.route("/")
+#@app.route("/home")
+#def home():
+#    return render_template("project-form.html")
---
+++ start.sh
@@ -0,0 +1,3 @@
+export FLASK_APP=framework.py
+export FLASK_ENV=development
+flask run
