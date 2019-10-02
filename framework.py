caf61a8b85607ad5c8a1bf5fb5808c3ff9acfbff
Jacob <jacobmen416@yahoo.com>
2019-10-02 10:25:39 -0500

    fixed folder name

--- framework.py
+++ framework.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-# only temp. replace with real templates once ready
-#@app.route("/")
-#@app.route("/home")
-#def home():
-#    return render_template("project-form.html")
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("project-form.html")
