efd81a890a18857e62f3a965eda931536ed451b8
shreyasharma <ssharm90@illinois.edu>
2019-10-20 17:33:58 -0500

    rendered to layout.html

--- framework.py
+++ framework.py
@@ -1,13 +1,24 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home", methods=['POST'])
 def home():
     if(request.method == 'POST'):
-        project_name = request.files['projectName']
-        project_description = request.values['projectDescription']
+        print(request.get_json())
+        project_name = request.form['projectName']
+        project_description = request.form['projectDescription']
         project_time = request.form['projectTime']
         project_language = request.form['projectLanguages']
-            
-    return "render_template("")"
+
+    return redirect(url_for("projects"))
+
+
+@app.route("/projects")
+def projects():
+    return render_template("layout.html")
+
+
+@app.route("/submit")
+def submit():
+    return render_template("project-form.html")
